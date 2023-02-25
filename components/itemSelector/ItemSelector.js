6022c1dd104b49b50a868cb32556c6f902e36063
luizfiorentino <lgfiorentino@gmail.com>
2023-02-25 15:08:38 +0100

    feat- menu bar items getting highlighted when clicked upon through useState

--- components/itemSelector/ItemSelector.js
+++ components/itemSelector/ItemSelector.js
@@ -1,12 +1,19 @@
 import React from "react";
 import styles from "./ItemSelector.module.css";
+import { useState } from "react";
 
 export default function ItemSelector(props) {
-  const propsI = props;
-  console.log("item selector props=", props.children);
+  console.log("item selector props=", props);
 
   return (
-    <h5 className={`${styles.main} ${styles[props.variant]}`}>
+    <h5
+      className={
+        props.item !== props.itemSelected
+          ? `${styles.main} ${styles[props.variant]}`
+          : `${styles.mainActive} ${styles[props.variant]}`
+      }
+      onClick={() => props.setItem(props.item)}
+    >
       {props.children}
     </h5>
   );
--- components/menuBar/MenuBar.js
+++ components/menuBar/MenuBar.js
@@ -2,8 +2,19 @@ import React from "react";
 import styles from "./MenuBar.module.css";
 import ItemSelector from "../itemSelector/ItemSelector";
 import data from "../../pages/data.json";
+import { useState } from "react";
 
 export default function MenuBar({ variant = "main", ...items }) {
+  const [itemSelected, setItemSelected] = useState(0);
+
+  const setItem = (item) => {
+    console.log("item selected->", itemSelected);
+
+    console.log("setItem", setItem);
+    setItemSelected(item);
+    console.log("item:::", item);
+  };
+
   console.log("variant: items:", items);
   return (
     <div
@@ -12,7 +23,13 @@ export default function MenuBar({ variant = "main", ...items }) {
     >
       {variant === "mobile"
         ? items.items.map((item) => (
-            <ItemSelector key={item} variant="pageMenu">
+            <ItemSelector
+              key={item}
+              item={items.items.indexOf(item)}
+              variant="pageMenu"
+              setItem={setItem}
+              itemSelected={itemSelected}
+            >
               {item}
             </ItemSelector>
           ))
