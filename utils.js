86457621d23369c37a47eb61f8c53d41233d323f
luizfiorentino <lgfiorentino@gmail.com>
2023-03-08 10:03:42 +0100

    contentful credentials replaced to .env

--- utils.js
+++ utils.js
@@ -1,8 +1,17 @@
 import { createClient } from "contentful";
+// import { config } from "dotenv";
 
+// config();
+
+// const client = createClient({
+//   space: "9f26325ziuts",
+//   accessToken: "Ta_O5j4YDaSFLl3EpV4xK0kzLWT9f1tgM0IOeeHpGgQ",
+
+// });
+console.log("process.env->", process.env.CONTENTFUL_API_TOKEN);
 const client = createClient({
-  space: "9f26325ziuts",
-  accessToken: "Ta_O5j4YDaSFLl3EpV4xK0kzLWT9f1tgM0IOeeHpGgQ",
+  space: `${process.env.CONTENTFUL_SPACE_ID}`,
+  accessToken: `${process.env.CONTENTFUL_API_TOKEN}`,
 });
 
 const getPlanetContent = async () => {
