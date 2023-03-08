76d5dd7c7b1317d80eeeba5396cdd1cfe59c263c
luizfiorentino <lgfiorentino@gmail.com>
2023-03-08 10:04:58 +0100

    credentials deleted

--- utils.js
+++ utils.js
@@ -1,13 +1,5 @@
 import { createClient } from "contentful";
-// import { config } from "dotenv";
 
-// config();
-
-// const client = createClient({
-//   space: "9f26325ziuts",
-//   accessToken: "Ta_O5j4YDaSFLl3EpV4xK0kzLWT9f1tgM0IOeeHpGgQ",
-
-// });
 console.log("process.env->", process.env.CONTENTFUL_API_TOKEN);
 const client = createClient({
   space: `${process.env.CONTENTFUL_SPACE_ID}`,
