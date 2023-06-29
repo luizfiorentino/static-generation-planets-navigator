8bd7a28c220613663e217c8d17196978a0b4b09c
luizfiorentino <lgfiorentino@gmail.com>
2023-06-29 10:37:49 +0200

    Hardcoded link to Wikipedia added to Planet Page

--- components/sourceLink/SourceLink.js
+++ components/sourceLink/SourceLink.js
@@ -8,9 +8,29 @@ export default function SourceLink() {
       Source :{" "}
       <span className={styles.link}>
         {" "}
-        <span className={styles.wikiText}>Wikipedia</span>
+        <span className={styles.wikiText}>
+          <a
+            href="https://en.wikipedia.org/wiki/Solar_System"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Wikipedia
+          </a>
+        </span>
       </span>
       <img src={Icon.src} alt="a link icon" />
     </p>
   );
+}
+{
+  /* <p className={styles.main}>
+    Source:{" "}
+    <span className={styles.link}>
+      {" "}
+      <a href="https://en.wikipedia.org" target="_blank" rel="noopener noreferrer">
+        <span className={styles.wikiText}>Wikipedia</span>
+      </a>
+    </span>
+    <img src={Icon.src} alt="a link icon" />
+  </p> */
 }
